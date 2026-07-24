9ea2ec378e5f697f99be22a0526ef617ceb5e8fb
Francis Mungai <117128126+francismungai@users.noreply.github.com>
2026-07-24 11:14:49 -0500

    Add: max-water-container - submission-2

---
+++ Data Structures & Algorithms/max-water-container/submission-2.rs
@@ -0,0 +1,29 @@
+impl Solution {
+    pub fn max_area(heights: Vec<i32>) -> i32 {
+        let mut left = 0;
+        let mut right = heights.len() - 1;
+        let mut area = 0;
+        let mut max_area = 0;
+
+        while left < right {
+            let width = (right - left) as i32;
+
+            match heights[left].cmp(&heights[right]) {
+                Ordering::Less | Ordering::Equal => {
+                    area = width * heights[left];
+                    left += 1;
+                },
+                Ordering::Greater => {
+                    area = width * heights[right];
+                    right -= 1;
+                },
+            }
+
+            if area > max_area {
+                max_area = area;
+            }
+        }
+
+        max_area
+    }
+}
